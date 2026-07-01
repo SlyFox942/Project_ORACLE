4904ca825da71ec02f4dddbd624735d134d9d8e1
Aikin-V <Vaikin88@yahoo.com>
2026-07-01 09:16:15 -0400

    Add missing Analytics Lab page

---
+++ pages/Analytics_Lab.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+from components.footer import show_footer
+
+st.title("📈 Analytics Lab")
+st.caption("Project ORACLE Analytics Workspace")
+
+st.info("Analytics Lab is under development.")
+
+show_footer()
